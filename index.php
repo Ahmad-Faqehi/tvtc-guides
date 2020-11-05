9307a63fa5cfe5d4272bbe34785f4a2e7d990bf0
Ahmad-Faqehi <ahmadpsn7@gmail.com>
2020-11-05 05:41:04 +0300

    add order page

---
+++ index.php
@@ -0,0 +1,341 @@
+
+<!DOCTYPE html>
+<html lang="ar" >
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="app landing page,business,finance,corporate,landing page,ui,ux">
+    <meta name="author" content="Yucel Yilmaz">
+    <meta name="robots" content="noindex, nofollow">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>TVTC Guides</title>
+    <!--// Icons //-->
+    <link rel="stylesheet" href="fonts/flat_icons/flaticon.css">
+    <link rel="stylesheet" href="fonts/flat-icons2/flaticon.css">
+    <link rel="stylesheet" href="fonts/font_awesome/css/all.css">
+    <!--// Google Fonts //-->
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i&display=swap&subset=latin-ext" rel="stylesheet">
+    <!--// FrameWorks //-->
+    <link rel="stylesheet" href="css/frameworks.css">
+    <!--// Theme Main Js //-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mystyle.css">
+</head>
+
+<body data-spy="scroll" data-target="#fixedNavbar" data-offset="70">
+
+<!--// Header Start //-->
+<header class="header fixed-top">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg p-0">
+            <a class="navbar-brand btn btn-link" href="#">
+
+                <img src="img/bg/tvtc.png" alt="Logo" class=" w-50 img-fluid logo-transparent ">
+                <img src="img/bg/tvtc.png" alt="Logo" class=" w-50 img-fluid logo-normal">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#fixedNavbar" aria-controls="fixedNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="togler-icon-inner">
+                        <span class="line-1"></span>
+                        <span class="line-2"></span>
+                        <span class="line-3"></span>
+                    </span>
+            </button>
+            <div class="collapse navbar-collapse main-menu justify-content-end" id="fixedNavbar" >
+                <ul class="navbar-nav" style="text-align: initial;" dir="rtl">
+
+
+                    <li class="nav-item">
+                        <a class="nav-link fonty" href="#" data-scroll-nav="1" style="font-size: 17px;">الرئيسية</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fonty" href="login.html"   style="font-size: 17px;">حسابي</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fonty" href="soon.html"  style="font-size: 17px;">تواصل معانا</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link menu-link fonty" href="soon.html"  style="font-size: 17px;">أتصل بنا</a>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
+<!--// Header End  //-->
+
+<!--// Hero Start //-->
+
+<section id="home" class="jarallax" data-scroll-index="1" data-jarallax="" data-speed="0.6s" style="background-image: url(img/bg/bg-TVTC.jpeg); direction: rtl">
+    <div class="banner-wrap">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <div class="banner-content">
+                        <h1 class="wow fadeInUp text-center " style="letter-spacing: 3px;" data-wow-delay="0.1s">TVTC Guides</h1>
+                        <p class="wow fadeInUp h5 text-center fonty spc" data-wow-delay="0.2s">
+                            موقع يقدم عدة أقسام من الخدمات تخدم الكلية التقنية
+                        </p>
+                        <div class="holder-link wow fadeInUp text-center" data-wow-delay="0.3s" data-scroll-nav="2">
+                            <a href="#"  class="default-button fonty">
+                               عرض الخدمات
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="#" class="scroll-down-btn" data-scroll-nav="2">
+        <i class="fa fa-arrow-down"></i>
+    </a>
+</section>
+<!--// Hero End //-->
+
+<!-- Services Start -->
+<section class="section pb-minus-1" id="services" data-scroll-index="2">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="section-heading">
+                    <h2 class="section-title fonty">الخدمات</h2>
+
+<!--                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolor fuga, magnam et sed quidem.</p>-->
+                </div>
+            </div>
+        </div>
+        <div class="tab-link-wrap">
+            <div class="row">
+                <div class="col-md-6 col-lg-6 offset-md-3 offset-lg-3 tab-link-item wow fadeInLeft pb-3" data-wow-delay="0s">
+                    <a href="login.html">
+                    <div class="tab-link-inner">
+                        <span class="flaticon-user"></span>
+                        <h6 class="fonty-par">إدارة الحساب</h6>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-md-6 col-lg-6 offset-md-3 offset-lg-3 tab-link-item wow fadeInRight pb-3" data-wow-delay="0.1s">
+                    <a href="soon.html">
+                    <div class="tab-link-inner">
+                        <span class="flaticon-checklist"></span>
+                        <h6 class="fonty-par">الطلبات</h6>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-md-6 col-lg-6 offset-md-3 offset-lg-3 tab-link-item wow fadeInLeft pb-3" data-wow-delay="0.3s">
+                    <a href="soon.html">
+                    <div class="tab-link-inner">
+                        <span class="flaticon-contact"></span>
+                        <h6 class="fonty-par">التواصل</h6>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-md-6 col-lg-6 offset-md-3 offset-lg-3 tab-link-item wow fadeInRight pb-3" data-wow-delay="0.3s">
+                    <a href="soon.html">
+                    <div class="tab-link-inner">
+                        <span class="flaticon-lost-items"></span>
+                        <h6 class="fonty-par">المفقودات</h6>
+                    </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--// Services End  //-->
+
+<!--// How It Work Start //-->
+<section id="how-it-work" class="section" style="direction: rtl">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12 col-lg-6 text-right">
+                <div class="how-it-works-inner">
+                    <h4 class="fonty mo-center">كيف يعمل الموقع؟</h4>
+                    <p class="fonty-par mo-center">
+                        هنا نص قصير عن آلية عمل الموقع و الفكرة المطروحة من هذا المشروع, تندرج تحت هذه الاقسام
+                    </p>
+                    <ul>
+                        <li class="how-it-work-item">
+                            <div class="how-it-work-icon">
+                                <i class="fas fa-tasks"></i>
+                            </div>
+                            <div class="how-it-work-body">
+                                <h6 class="fonty-nav" >قسم الطلبات</h6>
+                                <p class="fonty-par">
+                                    يمكن من خلال هذا القسم طلب الدعم الفني - الطبيبة - الأمن - مسؤولة المفاتيح وذلك لسهولة الوصول اليه
+                                </p>
+                            </div>
+                        </li>
+                        <li class="how-it-work-item">
+                            <div class="how-it-work-icon">
+                                <i class="fas fa-question"></i>
+                            </div>
+                            <div class="how-it-work-body">
+                                <h6 class="fonty-nav" >قسم المفقودات</h6>
+                                <p class="fonty-par">
+                                    عبارة عن لائحة إعلانات يمكن من خلالها البحث عن المفقودات
+                                </p>
+                            </div>
+                        </li>
+                        <li class="how-it-work-item mb-0">
+                            <div class="how-it-work-icon">
+                                <i class="far fa-comment-dots"></i>
+                            </div>
+                            <div class="how-it-work-body">
+                                <h6 class="fonty-nav" >قسم التواصل</h6>
+                                <p class="fonty-par">
+                                    من هذا القسم يمكن من خلاله التواصل السريع مع المسؤولين
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <div class="how-it-work-img">
+                    <img src="img/bg/how-it-work.png" alt="" class="img-fluid mo-pt-15">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--// How It Work End //-->
+
+<!--/ Counter Start /-->
+<section class="section pb-minus-70 " id="counters">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="section-heading white-section-text">
+                    <h2 class="section-title fonty">إحصائيات</h2>
+                    <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolor fuga, magnam et sed quidem.</p> -->
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInUp" data-wow-delay="0.2s">
+                <div class="counter-item">
+                    <div class="counter-header">
+                        <i class="fas fa-hand-holding-heart"></i>
+                    </div>
+                    <div class="counter-body">
+                        <h2 class="counter">24</h2>
+                        <span class="fonty-par">عدد الطلبات</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInUp" data-wow-delay="0s">
+                <div class="counter-item">
+                    <div class="counter-header">
+                        <i class="mdi mdi-account-heart"></i>
+                    </div>
+                    <div class="counter-body">
+                        <h2 class="counter">178</h2>
+                        <span class="fonty-par">عدد المستفيدين</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="counter-item">
+                    <div class="counter-header">
+                        <i class="mdi mdi-ticket"></i>
+                    </div>
+                    <div class="counter-body">
+                        <h2 class="counter">34</h2>
+                        <span class="fonty-par">عدد مرات التوصل</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--// Counter End //-->
+
+
+
+<!--// Watch Video Start //-->
+<section class="section jarallax"  data-jarallax="" data-speed="0.6s">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 ">
+                <div class="section-heading white-section-text">
+                    <h2 class="section-title text-dark fonty">شاهد فديو الشرح</h2>
+                    <p class="fonty-par text-dark">فديو شرح طريقة الاستفادة من الخدمات الموقع</p>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="video-inner-wrap">
+                    <img src="img/bg/maxresdefault.jpg" alt="Video image" class="img-fluid">
+                    <a href="https://www.youtube.com/watch?v=vfRykT6eFQ0" class="video-button popup-youtube"><i class="fa fa-play"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--// Watch Video End //-->
+
+
+
+
+<!--// Footer Start //-->
+<footer class="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            <p class="copyright-text text-center fonty-par">مشروع متدربات الكلية التقنية الرقمية بالرياض - إشراف المدربة <b>أ. ايمان الغامدي</b></p>
+        </div>
+    </div>
+</footer>
+<!--// Footer End //-->
+
+<!--<a href="#" data-scroll-goto="1" class="scroll-top-btn"><i class="fa fa-arrow-up"></i></a>-->
+<!--// Scroll Top Btn  //-->
+
+<div class="preloader-wrap">
+    <div class="preloader-inner">
+        <div id="loader"></div>
+    </div>
+</div>
+<!--// Preloader  //-->
+
+<!--// Scripts //-->
+<script src="js/jquery.js"></script>
+<script src="js/plugins.js"></script>
+<!--// Theme Main Js // -->
+<script src="js/main.js"></script>
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = '28e1e7cc790aae898633c1bc56669ab314f53628';
+    window.smartsupp||(function(d) {
+        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+        c.type='text/javascript';c.charset='utf-8';c.async=true;
+        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+    })(document);
+</script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-68483271-3"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-68483271-3');
+</script>
+
+</body>
+
+</html>
